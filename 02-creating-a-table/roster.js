a833b5b70f95a3aa0fd5e8b4b23f16d572d12504
adam <adammullineux@hotmail.com>
2018-01-24 00:26:07 +0000

    commented the file with notes from video

--- 02-creating-a-table/roster.js
+++ 02-creating-a-table/roster.js
@@ -1,10 +1,15 @@
-
 /* Creating a table
  *
  * This script shall create a table from a tab-separated value text document.
+ * Note that this file follows a template for D3.js files which this course 
+ * will follow repeatedly for data driven controllers
  */
 
-/* Store the data in an array. Starts off empty.
+/* Positions hashmap for lookup */
+var positions = { G: "Goalkeeper", D: "Defender", M: "Midfielder", F: "Forward" }
+
+/* Part 1: Program variables
+ * Store the data in an array. Starts off empty.
  * This is usually NOT global, but making global here for demonstration purposes.
  */
 var data = [];
@@ -15,8 +20,8 @@ var data = [];
  * The resulting table element is stored in the variable "table."
  */
 var table = d3.select('#roster')
-  .append('table')
-  .classed('table',true);
+    .append('table')
+    .classed('table', true);
 
 /* Append <thead><tr></tr></thead> to the above table.
  * The inner tr element is stored in the "thead" variable.
@@ -26,23 +31,66 @@ var thead = table.append('thead').append('tr');
 /* Append <tbody></tbody> to the table and store it in the "tbody" variable. */
 var tbody = table.append('tbody');
 
-/* Function to reload the data from the data file.
+/* Part 2: the reload function (where the data gets loaded and sorted)
+ * Function to reload the data from the data file.
  * Call the redraw() function after the data is loaded to drive the drawing of the data.
  * We'll be filling this in during the lesson.
  */
 var reload = function() {
-  console.log("reload() called.");
-  redraw();
+    d3.tsv('afcw-roster.tsv', function(rows) {
+            data = rows;
+            data.forEach(function(d) {
+                d.Pos = positions[d.Pos];
+            }); /*map in the positions using the lookup to positions variable*/
+            redraw();
+        })
+        /*console.log("Congrats!  You have loaded the data!");*/
 };
+/*note the console log to show the script has reached this stage
+check the browser console for this message!
+Also added in the tsv file using d3 tsv function, 
+which then parses the file into an array of ordered maps for each record
+The callback function then brings in the rows as a parameter
+the function also redraws to bind each record to the DOM when implemented
 
-/* Function to redraw the table.
+NOTE: if you get the code wrong and the data doesn't load, clear the cache in the 
+browser console by using window.location.reload(true)
+This refreshes the page direct from the files and ignores any caching
+LATER: reload() works just as well*/
+
+/* Part 3: the redraw function (where the data gets bound and rendered)
+ * Function to redraw the table.
  * It's good practice to keep the data input and drawing funcitons separate.
  * We'll be filling this in during the lesson.
  */
 var redraw = function() {
-  console.log("redraw() called.");
+    thead.selectAll("th") /*select virtual th elements*/
+        .data(d3.map(data[0]).keys().slice(2)) /*map & assign the first record's keys (as in key:value) as their data*/
+        .enter()
+        .append("th") /*enter the th elements where there was no data before, i.e. all*/
+        .text(function(d) { return d; }); /*with the appropriate text, as per chapter 1*/
+    /*added the slice in there to take the first two columns off the header*/
+
+    var rows = tbody.selectAll("tr")
+        .data(data); /*select the virtual table row elements and assign the data*/
+
+    rows.enter().append("tr"); /*for each row which doesn't already exist, i.e. all, create a tr element*/
+    rows.exit().remove(); /*handle removing any unwanted rows on page reload, just in case*/
+
+    var cells = rows.selectAll("td")
+        .data(function(row) { return d3.map(row).values().slice(2); });
+    /*each tr row has its td cell elements selected and appended on enter
+    note this is the alternate to the keys*/
+    /*also note slice to take the first two columns off the td section too*/
+
+    cells.enter().append("td");
+    cells.text(function(d) { return d; });
+
+    /*console.log("Congrats!  You have drawn the data!");*/
 };
+/*note the console log to show the script has reached this stage
+check the browser console for this message to be sure it's working!*/
 
-/* Call reload() once the page and script have loaded to get the controller script started. */
+/* Finally we call the reload function on the page to get everything started (crank it up!)
+ * Call reload() once the page and script have loaded to get the controller script started. */
 reload();
-
